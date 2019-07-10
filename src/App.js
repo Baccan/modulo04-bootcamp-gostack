ae9efb35acf3cb30a16c71af8b3007457f0b0ca8
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-07-10 10:56:32 -0300

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import profile from "./assets/Kuramon.png";
+import TechList from "./components/TechList";
 
 function App() {
-  return <img width="200" src={profile} alt="Kuramon" />;
+  return <TechList />;
 }
 
 export default App;

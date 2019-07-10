3f925417c5513b89e0ce87e1d2a6c20c5a83d911
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-07-10 10:45:26 -0300

    Importando Imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import profile from "./assets/Kuramon.png";
+
 function App() {
-  return <h1>Hello Rocketseat</h1>;
+  return <img width="200" src={profile} alt="Kuramon" />;
 }
 
 export default App;

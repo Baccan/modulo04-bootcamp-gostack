1b9c1ac37c964dff50180a878341b536b2ca150b
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-07-11 10:33:04 -0300

    Ciclo de vida dos componentes e LocalStorage

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -12,8 +12,32 @@ class TechList extends Component {
   // estados para o componente
   state = {
     newTech: "",
-    techs: ["Node.js", "ReactJS", "React Native"]
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  /**
+    componentDidUpdate(prevProps, prevState) {
+      // this.props, this.state
+    }
+   */
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {}
 
   // Funções próprias, que não são do react, devem ser arrow functions
   // caso contrário, a função não teria acesso ao this do componente e estado
